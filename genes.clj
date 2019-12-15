bb4eb92aab45f840081aa6f928de27ffcc0c48b7
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-15 22:05:48 +0000

    Muckin' about in clojure

--- genes.clj
+++ genes.clj
@@ -5,20 +5,38 @@
 ;; as part of the optimisation.
 
 ;; GENOME: a list of genes
-(defvar genome '((add 1)
-                 (multiply 2)))
 
-;; ORGANISM: a collection of genes working together
-(defvar organism '((pop genome) (pop (rest genome))))
-(print "here")
+(defrecord Gene [function name])
+(def genome [
+             (->Gene + "add")
+             (->Gene - "subtract")
+             (->Gene * "multiply")
+             (->Gene / "divide")
+             ])
 
-(defun eval-organism (organism x)
-  (defun iter (l xi)
-    (print l)
-    (if (not (rest l)) 
-      (eval (pop (pop l)) xi (pop (pop (rest l))))
-      (eval (pop (pop l)) (iter (rest l)) (pop (pop (rest l))))))
-  (iter 'organism x))
+;;(println ((:function (first genome)) 1 2))
 
-(eval-organism organism 4)
+;;;; ;; ORGANISM: a collection of genes working together
+(def an-organism '((get genome 1)))
+
+(defn try-organism [organism input]
+  ;; function to run organism on input
+  (cond ((record? organism) (println "is record"))
+        (1 (println "else"))))
+
+(try-organism (get genome 1) 8)
+
+;;;; (print "here")
+;;;; 
+;;;; (defn eval-organism [organism x]
+;;;;   (defn iter [l xi]
+;;;;     (print l)
+;;;;     (if (not (rest l)) 
+;;;;       (eval (pop (pop l)) xi (pop (pop (rest l))))
+;;;;       (eval (pop (pop l)) (iter (rest l)) (pop (pop (rest l))))))
+;;;;   (iter 'organism x))
+;;;; 
+;;;; (eval-organism organism 4)
+
+
 ;; genes.clj ends here
